cb327cb20160a273d0ecaaa8a804a9a26ad6d1ce
JJedut <131999253+JJedut@users.noreply.github.com>
2025-04-27 22:20:13 +0200

    Dodaje QR

--- frontend/src/components/UserPanel.jsx
+++ frontend/src/components/UserPanel.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import MapElement from './MapElement';
+import QrScanner from "./QrScanner.jsx";
 
 function UserPanel() {
   const [nickname, setNickname] = useState("");
@@ -10,6 +11,7 @@ function UserPanel() {
   const [newGroupName, setNewGroupName] = useState("");
   const [groupCreated, setGroupCreated] = useState(null);
   const [groupName, setGroupName] = useState("");
+  const [expandedTaskId, setExpandedTaskId] = useState(null);
   const [groupUsers, setGroupUsers] = useState([]);
 
   const handleLogin = async () => {
@@ -60,6 +62,15 @@ function UserPanel() {
     }
   };
 
+  const handleScanResult = (result) => {
+    console.log("Otrzymano wynik skanowania w komponencie nadrzędnym:", result);
+    alert(`Zeskanowano: ${result}`);
+  };
+
+  const toggleTask = (taskId) => {
+    setExpandedTaskId((prevId) => (prevId === taskId ? null : taskId));
+  };
+
   return (
     <div>
       {!isLoggedIn ? (
@@ -95,21 +106,33 @@ function UserPanel() {
           )}
         </div>
       ) : (
-        <div>
-          <h2>Witaj, {nickname}!</h2>
-          <p>
-            Grupa: {groupName} (Kod: {groupCode})
-          </p>
-          <MapElement tasks={tasks} />
-          <h3>Lista Tasków:</h3>
-          <ul>
-            {tasks.map((task) => (
-              <li key={task._id}>
-                {task.name} - {task.description} ({task.location.lat},{" "}
-                {task.location.lng})
-              </li>
-            ))}
-          </ul>
+          <div>
+            <h2>Witaj, {nickname}!</h2>
+            <p>
+              Grupa: {groupName} (Kod: {groupCode})
+            </p>
+            <MapElement tasks={tasks}/>
+            <h3>Lista Tasków:</h3>
+            <div className="task-list">
+              {tasks.map((task) => (
+                  <div key={task._id} className="task-list-element">
+                    <div
+                        onClick={() => toggleTask(task._id)}
+                        style={{cursor: 'pointer', fontWeight: 'bold', padding: '8px', borderBottom: '1px solid #ccc'}}
+                    >
+                      {task.name}
+                    </div>
+
+                    {expandedTaskId === task._id && (
+                        <div style={{padding: '8px', backgroundColor: '#f9f9f9'}}>
+                          <div>{task.description}</div>
+                          <div>Location: ({task.location.lat}, {task.location.lng})</div>
+                          <QrScanner onScanSuccess={handleScanResult}/>
+                        </div>
+                    )}
+                  </div>
+              ))}
+            </div>
           <h3>Członkowie grupy:</h3>
           <ul>
             {groupUsers.map((user) => (
